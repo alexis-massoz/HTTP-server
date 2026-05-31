1b18c5545598690565924428c8c0cfe138d9b4ed
alexis-massoz <alexismassoz@gmail.com>
2026-05-31 21:04:47 +0200

    middleware added

---
+++ src/api/middlewares.ts
@@ -0,0 +1,11 @@
+import type { NextFunction, Request, Response } from "express";
+
+export function middlewareLogResponses (req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        const status = res.statusCode;
+        if (status !== 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./api/middlewares.js";
 
 const app = express();
 const PORT = 8080;
+
+app.use(middlewareLogResponses);
 
 app.use("/app", express.static("./src/app"));
 
@@ -11,7 +14,4 @@ app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-
-
 
